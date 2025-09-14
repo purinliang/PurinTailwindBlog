11a620d9e7bf99dcb8e696228310423e00ad1ec2
purinliang <purinliang@gmail.com>
2025-09-15 02:30:50 +1000

    minimize build dist

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -10,18 +10,18 @@ const siteMetadata = {
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  x: 'https://twitter.com/x',
+  // mastodon: 'https://mastodon.social/@mastodonuser',
+  // email: 'address@yoursite.com',
+  // github: 'https://github.com',
+  // x: 'https://twitter.com/x',
   // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
-  medium: 'https://medium.com',
-  bluesky: 'https://bsky.app/',
+  // facebook: 'https://facebook.com',
+  // youtube: 'https://youtube.com',
+  // linkedin: 'https://www.linkedin.com',
+  // threads: 'https://www.threads.net',
+  // instagram: 'https://www.instagram.com',
+  // medium: 'https://medium.com',
+  // bluesky: 'https://bsky.app/',
   locale: 'en-US',
   // set to true if you want a navbar fixed to the top
   stickyNav: false,
@@ -59,36 +59,36 @@ const siteMetadata = {
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
-    giscusConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
-      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
-      // theme example: light, dark, dark_dimmed, dark_high_contrast
-      // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
-      // theme when dark mode
-      darkTheme: 'transparent_dark',
-      // If the theme option above is set to 'custom`
-      // please provide a link below to your custom theme css file.
-      // example: https://giscus.app/themes/custom_example.css
-      themeURL: '',
-      // This corresponds to the `data-lang="en"` in giscus's configurations
-      lang: 'en',
-    },
+    provider: '', // supported providers: giscus, utterances, disqus
+    // giscusConfig: {
+    //   // Visit the link below, and follow the steps in the 'configuration' section
+    //   // https://giscus.app/
+    //   repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+    //   repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+    //   category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+    //   categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+    //   mapping: 'pathname', // supported options: pathname, url, title
+    //   reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+    //   // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+    //   metadata: '0',
+    //   // theme example: light, dark, dark_dimmed, dark_high_contrast
+    //   // transparent_dark, preferred_color_scheme, custom
+    //   theme: 'light',
+    //   // theme when dark mode
+    //   darkTheme: 'transparent_dark',
+    //   // If the theme option above is set to 'custom`
+    //   // please provide a link below to your custom theme css file.
+    //   // example: https://giscus.app/themes/custom_example.css
+    //   themeURL: '',
+    //   // This corresponds to the `data-lang="en"` in giscus's configurations
+    //   lang: 'en',
+    // },
   },
   search: {
-    provider: 'kbar', // kbar or algolia
-    kbarConfig: {
-      searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`, // path to load documents to search
-    },
+    provider: '', // kbar or algolia
+    // kbarConfig: {
+    //   searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`, // path to load documents to search
+    // },
     // provider: 'algolia',
     // algoliaConfig: {
     //   // The application ID provided by Algolia
